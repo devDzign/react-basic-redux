245d9bf6d2c7d2f86519fbe671e4f5f8ab1264cf
mourad <chabour.mourad87@gmail.com>
2020-04-19 21:55:43 +0200

    create header component

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import Header from "../components/Header";
 
 function App() {
   return (
     <>
-        <div className="container">
-            <h1>React start</h1>
-        </div>
+        <Header/>
 
     </>
   );
